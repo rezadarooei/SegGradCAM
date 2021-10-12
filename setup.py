6303b83261319ab3740cd5d24fcbfde84c2664c5
Reza Darooei <33072741+rezadarooei@users.noreply.github.com>
2021-10-12 15:27:07 +0330

    Update setup.py

--- setup.py
+++ setup.py
@@ -26,7 +26,7 @@ setup (
     install_requires=[
         'csbdeep,
         'tensorflow,
-       'tensorflow-gpu,
+#        'tensorflow-gpu,
         #'keras-gpu>=2.0.0',
         'opencv-python',
     ],
